b8494853245ade3799d6773d2fba3e3857d1d412
Christine Lasquellec <christine.lasquellec@isen.yncrea.fr>
2020-04-21 15:40:00 +0200

    add WithFeedback extensions for UIActionHighlightButton

--- Controls/HighlightButton/UIActionHighlightButton.cs
+++ Controls/HighlightButton/UIActionHighlightButton.cs
@@ -15,11 +15,19 @@ namespace Xmf2.iOS.Extensions.Controls.HighlightButton
 		protected override void OnHighlighted() => Animate(ToHighlightedAnimationDuration, () => ToHighlightedAnimation(this), () => { });
 		protected override void OnUnhighlighted() => Animate(FromHighlightedAnimationDuration, () => FromHighlightedAnimation(this), () => { });
 
-		#region Extensions
-
-		public UIActionHighlightButton WithFadeFeedbackRedirect(float fadeIn, params UIView[] onViews)
+		protected override void Dispose(bool disposing)
 		{
-			return WithFeedBack(_ =>
+			ToHighlightedAnimation = null;
+			FromHighlightedAnimation = null;
+			base.Dispose(disposing);
+		}
+	}
+
+	public static class ActionHighlightButtonExtensions
+	{
+		public static TActionHighlightButton WithFadeFeedbackRedirect<TActionHighlightButton>(this TActionHighlightButton button, float fadeIn, params UIView[] onViews) where TActionHighlightButton : UIActionHighlightButton
+		{
+			return button.WithFeedBack(_ =>
 			{
 				foreach (var view in onViews)
 				{
@@ -34,42 +42,33 @@ namespace Xmf2.iOS.Extensions.Controls.HighlightButton
 			});
 		}
 
-		public UIActionHighlightButton WithFadeFeedbackOnSuperview(float fadeIn)
+		public static TActionHighlightButton WithFadeFeedbackOnSuperview<TActionHighlightButton>(this TActionHighlightButton button, float fadeIn) where TActionHighlightButton : UIActionHighlightButton
 		{
-			return WithFeedBack(button => button.Superview?.FadeTo(fadeIn), button => button.Superview?.FadeTo(1f));
+			return button.WithFeedBack(button => button.Superview?.FadeTo(fadeIn), button => button.Superview?.FadeTo(1f));
 		}
 
-		public UIActionHighlightButton WitFeedbackOnSuperview(UIColor normalBackgroundColor, UIColor highlightBackgroundColor)
+		public static TActionHighlightButton WitFeedbackOnSuperview<TActionHighlightButton>(this TActionHighlightButton button, UIColor normalBackgroundColor, UIColor highlightBackgroundColor) where TActionHighlightButton : UIActionHighlightButton
 		{
-			return WithFeedBack(button => button.Superview.WithBackgroundColor(normalBackgroundColor), button => button.Superview.WithBackgroundColor(highlightBackgroundColor));
+			return button.WithFeedBack(button => button.Superview.WithBackgroundColor(normalBackgroundColor), button => button.Superview.WithBackgroundColor(highlightBackgroundColor));
 		}
 
-		public UIActionHighlightButton WithFeedbackOnSelf(UIColor normalBackgroundColor, UIColor highlightBackgroundColor)
+		public static TActionHighlightButton WithFeedbackOnSelf<TActionHighlightButton>(this TActionHighlightButton button, UIColor normalBackgroundColor, UIColor highlightBackgroundColor) where TActionHighlightButton : UIActionHighlightButton
 		{
-			return WithFeedBack(button => button.WithBackgroundColor(highlightBackgroundColor), button => button.WithBackgroundColor(normalBackgroundColor));
+			return button.WithFeedBack(button => button.WithBackgroundColor(highlightBackgroundColor), button => button.WithBackgroundColor(normalBackgroundColor));
 		}
 
-		public UIActionHighlightButton WithFeedBack(Action<UIActionHighlightButton> toHighlightedAnimation, Action<UIActionHighlightButton> fromHighlightedAnimation)
+		public static TActionHighlightButton WithFeedBack<TActionHighlightButton>(this TActionHighlightButton button, Action<UIActionHighlightButton> toHighlightedAnimation, Action<UIActionHighlightButton> fromHighlightedAnimation) where TActionHighlightButton : UIActionHighlightButton
 		{
-			ToHighlightedAnimation = toHighlightedAnimation;
-			FromHighlightedAnimation = fromHighlightedAnimation;
-			return this;
+			button.ToHighlightedAnimation = toHighlightedAnimation;
+			button.FromHighlightedAnimation = fromHighlightedAnimation;
+			return button;
 		}
 
-		public UIActionHighlightButton WithHighlightedAnimationDuration(double toHighlightedDuration, double fromHighlightedDuration)
+		public static TActionHighlightButton WithHighlightedAnimationDuration<TActionHighlightButton>(this TActionHighlightButton button, double toHighlightedDuration, double fromHighlightedDuration) where TActionHighlightButton : UIActionHighlightButton
 		{
-			ToHighlightedAnimationDuration = toHighlightedDuration;
-			FromHighlightedAnimationDuration = fromHighlightedDuration;
-			return this;
-		}
-
-		#endregion
-
-		protected override void Dispose(bool disposing)
-		{
-			ToHighlightedAnimation = null;
-			FromHighlightedAnimation = null;
-			base.Dispose(disposing);
+			button.ToHighlightedAnimationDuration = toHighlightedDuration;
+			button.FromHighlightedAnimationDuration = fromHighlightedDuration;
+			return button;
 		}
 	}
 }
